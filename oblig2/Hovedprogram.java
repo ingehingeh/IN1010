6cf02690e4e59ce258ed792abd8cfd9ded1a51a0
t-haakens <64080136+t-haakens@users.noreply.github.com>
2022-02-14 13:29:45 +0100

    oblig2 started

---
+++ oblig2/Hovedprogram.java
@@ -0,0 +1,9 @@
+// IN1010 vår 22 oblig 2: Legemidler
+// System som holder styr på leger, pasienter, resepter og legemidler.
+
+
+class Hovedprogram {
+    public static void main(String[] args) {
+        
+    }
+}
