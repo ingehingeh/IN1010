ca53e0e3fa3ab7639c63d9313efa622719aee828
t-haakens <64080136+t-haakens@users.noreply.github.com>
2022-02-16 08:46:31 +0100

    uke5 foreles notater: interface

---
+++ uke5/15.02-foreles-notater.java
@@ -0,0 +1,16 @@
+// Forelesning 15. Feb notater
+/* Subklasser 3:
+Interface - Grensesnitt, og invarianter
+Se notat: "Interface i Java"
+Kan se eksempelkode fra forelesning: BrukBibliotek eksempel
+
+2 måter å bruke interface:
+1) Multippel arv - arve fra flere
+- Samler felles egenskaper på tvers av klassehierarki
+- Kan ha flere roller 
+
+2) Definere en oppførsel/en rolle
+
+
+*/
+
